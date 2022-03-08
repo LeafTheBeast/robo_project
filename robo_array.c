1955f32cea3ca3953c61f7ec43f2ade5415ab2d3
Pierre <groenielp@gmail.com>
2022-03-08 21:09:56 +0100

    first commit

---
+++ robo_array.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+	char x[7][5];
+	char check[2];
+	int countA = 4;
+	int countB = 6;
+	int countC = 4;
+	
+	setbuf(stdout, NULL);
+	
+		for(int c = 0 ; c < 7; c++)
+		{
+			for(int r = 0 ; r < 5; r++)
+			{
+				x[c][r] = '-';
+			}
+		}
+	
+
+
+		for(int c = 0 ; c < 7; c++)
+		{
+			for(int r = 0 ; r < 5; r++)
+			{
+				
+				printf("%c" ,x[c][r]);
+				
+				if(r == 4)
+				{
+					printf("\n");
+				}
+			}
+
+		}
+
+		
+
+
+		
+		do
+		{
+			scanf("%c", check);
+		
+			if(check[0] != 'y')
+			{
+				if(countA != 0)
+				{	
+					x[0][countA] = '0';
+					countA--;
+				}
+		
+			}
+
+			for(int c = 0 ; c < 7; c++)
+			{
+				for(int r = 0 ; r < 5; r++)
+				{	
+				
+					printf("%c" ,x[c][r]);
+				
+					if(r == 4)
+					{
+						printf("\n");
+					}
+				}
+
+			}
+
+		}
+		while(check[0] != 'y');
+
+}
